ee560c0a809146c8deabef6e2d2fd55aaf112608
leones <228levvol822@gmail.com>
2022-09-10 20:16:53 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+
+$(function(){
+	$('.slider').slick({
+		arrows: false,
+		dots: true,
+		slidesToShow:3,
+		responsive: [
+			{
+				breakpoint: 841,
+				settings:{
+					slidesToShow:2,
+				}
+			},
+			{
+				breakpoint: 601,
+				settings:{
+					slidesToShow:1,
+
+				}
+			}
+		]
+		
+	});
+});
